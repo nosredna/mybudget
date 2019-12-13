1383c06b284e1c96f707d33641f81e0309b43404
Anderson Nascimento <anderson.nascimento@dtn.com>
2019-12-13 15:13:05 -0500

    changes for scalable apps course

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const logger = require('./logger');
 const argv = require('./argv');
 const port = require('./port');
 const setup = require('./middlewares/frontendMiddleware');
+const setupApi = require('./middlewares/api');
 const isDev = process.env.NODE_ENV !== 'production';
 const ngrok =
   (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
@@ -14,8 +15,7 @@ const ngrok =
 const { resolve } = require('path');
 const app = express();
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+setupApi(app);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
